b4073ec4764da7da4c99558ecf917083b823a287
温东霖 <57186816+Ricardowdl@users.noreply.github.com>
2025-12-10 16:33:44 +0800

    Time: 12 ms ms (13.10%) | Memory: NaN MB (96.07%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,10 +3,7 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    //在坚持一遍，刚刚应该是循环出错了
-    //头指针，问题在于会多运很多遍
-    //一个头部向后走，一个尾部向前，相同则跳出
-    //还需要一个n指数
+    // test update github
     let head = 0,
         n = 0;
     while(head < nums.length){
