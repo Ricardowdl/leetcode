2a877ac63ed1ad21f97d13a1e221996c1e42775a
温东霖 <57186816+Ricardowdl@users.noreply.github.com>
2025-12-17 14:08:44 +0800

    Time: 8 ms (18.92%) | Memory: 57.9 MB (19.35%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    const dp = new Array(prices.length).fill(0).map(v => new Array(2).fill(0));
+    dp[0][0] = 0, dp[0][1] = -prices[0];
+    for(let i = 1; i < prices.length; i++){
+        dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
+        dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i]);
+
+    }
+    return dp[prices.length-1][0]
+};
