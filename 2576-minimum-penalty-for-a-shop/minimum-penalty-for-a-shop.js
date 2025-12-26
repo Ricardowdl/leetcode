a28ddddcf5ea5d636a6f193ebbd531b0d74f8f21
温东霖 <57186816+Ricardowdl@users.noreply.github.com>
2025-12-26 12:54:04 +0800

    Time: 5 ms (100.00%) | Memory: 57.4 MB (100.00%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.js
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.js
@@ -1,40 +1,20 @@
-/**
- * @param {string} customers
- * @return {number}
- */
-var bestClosingTime = function (customers) {
-    const arr = [];
-    let preClose = 0;
-    let afterClose = 0;
-
-    //第一次循环 把初始的cost算出来
-    for (let i = 0; i <= customers.length; i++) {
-        if (customers[i] === 'Y') {
-            afterClose++
-        }
-    }
-
-    arr.push(preClose + afterClose)
-
-    for (let i = 1; i <= customers.length; i++) {
-        //此时关门
-        if (customers[i - 1] === 'Y') {
-            afterClose--
-        } else {
-            preClose++
-        }
-
-
-        arr.push(afterClose+preClose)
-    }
-    console.log(arr)
-
-    //找最小
+var bestClosingTime = function(customers) {
+    const n = customers.length;
+    let suf = 0;
+    let pre = 0;
+    let minCost = 0;
     let res = 0;
-    for (let i = arr.length - 1; i >= 0; i--) {
-        if (arr[i] <= arr[res]) {
-            res = i
+    
+    for (let i = 0; i <= n; i++) {
+        if (minCost > suf + pre) {
+            minCost = suf + pre;
+            res = i;
+        }
+        if (i < n && customers[i] === 'N') {
+            pre++;
+        } else if (i < n) {
+            suf--;
         }
     }
-    return res
+    return res;
 };
