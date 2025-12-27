70ba0017e07296e8f54aef59361ac6a3f9d0a60e
温东霖 <57186816+Ricardowdl@users.noreply.github.com>
2025-12-27 15:20:47 +0800

    Time: 15 ms (67.85%) | Memory: 58.3 MB (53.40%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function(amount, coins) {
+     const dp = new Array(amount + 1).fill(0);
+    const valid = new Array(amount + 1).fill(false);
+    dp[0] = 1;
+    valid[0] = true;
+    for (const coin of coins) {
+        for (let i = coin; i <= amount; i++) {
+            valid[i] |= valid[i - coin];
+        }
+    }
+    if (!valid[amount]) return 0;
+
+    for(const coin of coins){
+        for(let i = coin; i <= amount; i++){
+            dp[i] = dp[i-coin] + dp[i];
+        }
+    }
+    return dp[amount]
+};
