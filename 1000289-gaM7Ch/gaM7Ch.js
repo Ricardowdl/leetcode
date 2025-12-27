c210867a35d697f75ae76204371ef87eed7d2861
温东霖 <57186816+Ricardowdl@users.noreply.github.com>
2025-12-27 14:32:09 +0800

    Time: 117 ms (40.82%) | Memory: 62.6 MB (14.29%) - LeetSync

--- 1000289-gaM7Ch/gaM7Ch.js
+++ 1000289-gaM7Ch/gaM7Ch.js
@@ -1,16 +1,15 @@
-/**
- * @param {number[]} coins
- * @param {number} amount
- * @return {number}
- */
-var coinChange = function(coins, amount) {
+var coinChange = function (coins, amount) {
+    const max = amount + 1;
+    const dp = new Array(max).fill(max);
+    dp[0] = 0;
+    for(let i = 1; i <= amount; i++){
+        for(let j = 0; j < coins.length; j++){
+            if(coins[j] <= i){
 
-    let dp = new Array(amount+1).fill(Infinity); 
-    dp[0] = 0; // 凑出总额0 最低只需要 0 枚硬币
-    for(let num of coins) {
-        for(let j=num;j<=amount;j++) { 
-            dp[j] = Math.min(dp[j],dp[j-num]+1);
-        } 
+                dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+            }
+        }
     }
-     return dp[amount] == Infinity? -1 : dp[amount];
+    console.log(dp)
+    return dp[amount] > amount ? -1 : dp[amount];
 };
