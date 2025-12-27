b28eb2906233319f02498710667f0aa32ae453b5
温东霖 <57186816+Ricardowdl@users.noreply.github.com>
2025-12-27 14:36:34 +0800

    Time: 103 ms (46.94%) | Memory: 63 MB (14.29%) - LeetSync

--- 1000289-gaM7Ch/gaM7Ch.js
+++ 1000289-gaM7Ch/gaM7Ch.js
@@ -1,15 +1,13 @@
 var coinChange = function (coins, amount) {
-    const max = amount + 1;
-    const dp = new Array(max).fill(max);
+    const dp = new Array(amount+1).fill(amount+1);
     dp[0] = 0;
-    for(let i = 1; i <= amount; i++){
-        for(let j = 0; j < coins.length; j++){
-            if(coins[j] <= i){
-
-                dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+    for(const coin of coins){
+        for(let i = 1; i <= amount; i++){
+            if(coin <= i){
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1)
             }
         }
     }
     console.log(dp)
-    return dp[amount] > amount ? -1 : dp[amount];
-};
+    return dp[amount] > amount ? -1 : dp[amount]
+}
