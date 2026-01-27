bc3e03af777740d3a933d917d21cff24789315c5
温东霖 <57186816+Ricardowdl@users.noreply.github.com>
2026-01-27 18:21:38 +0800

    Time: 841 ms (0.00%) | Memory: 131.4 MB (100.00%) - LeetSync

---
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.js
@@ -0,0 +1,37 @@
+var minCost = function(n, edges) {
+    const g = Array.from({ length: n }, () => []);
+    for (const e of edges) {
+        const [x, y, w] = e;
+        g[x].push([y, w]);
+        g[y].push([x, 2 * w]);
+    }
+    
+    const dist = Array(n).fill(Infinity);
+    const visited = Array(n).fill(false);
+    dist[0] = 0;
+    const pq = new PriorityQueue((a, b) => {
+        return a[0] < b[0] ? -1 : 1;
+    });
+    pq.enqueue([0, 0]);
+    
+    while (!pq.isEmpty()) {
+        const [currentDist, x] = pq.dequeue();
+        if (x === n - 1) {
+            return currentDist;
+        }
+        
+        if (visited[x]) {
+            continue;
+        }
+        visited[x] = true;
+        
+        for (const [y, w] of g[x]) {
+            if (currentDist + w < dist[y]) {
+                dist[y] = currentDist + w;
+                pq.enqueue([dist[y], y]);
+            }
+        }
+    }
+    
+    return -1;
+};
